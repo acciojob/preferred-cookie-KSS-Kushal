32d6d04628573f88e13bb7ec19357a822381e771
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-04-14 19:34:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your JS code here. If required.
 const sizeHTML = document.getElementById('fontsize');
 const colorHTML = document.getElementById('fontcolor');
+const bodyHTML = document.getElementsByTagName("BODY")[0];
 
 const save = () => {
-	window.coockie = 
+	document.coockie = `size=${sizeHTML.value}; color=${colorHTML.value}`;
+	bodyHTML.style.color = colorHTML.value;
+	bodyHTML.style.fontSize = sizeHTML.value;
 }
