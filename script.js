7a2c4db102d11b1dd1e192e67d1c9d3e7cc2b6e5
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-04-14 19:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ const sizeHTML = document.getElementById('fontsize');
 const colorHTML = document.getElementById('fontcolor');
 const bodyHTML = document.getElementsByTagName("BODY")[0];
 
-const save = () => {
-	document.coockie = `size=${sizeHTML.value}; color=${colorHTML.value}`;
+const save = (e) => {
+	e.preventDefault();
 	bodyHTML.style.color = colorHTML.value;
-	bodyHTML.style.fontSize = sizeHTML.value;
+	bodyHTML.style.fontSize = `${sizeHTML.value}px`;
+	document.cookie = `size=${sizeHTML.value}; color=${colorHTML.value}`;
+}
+
+const cookie = document.cookie;
+const myObj = {};
+const data = cookie.split("; ");
+data.forEach((v)=>{
+	if(v.split("=")[0]=="color"){
+		myObj.color = v.split("=")[1];
+	}else if(v.split("=")[0]=="size"){
+		myObj.size = v.split("=")[1];
+	}
+})
+
+if(myObj.color){
+	bodyHTML.style.color = colorHTML.value;
+}
+if(myObj.size){
+	bodyHTML.style.fontSize = `${sizeHTML.value}px`;
 }
